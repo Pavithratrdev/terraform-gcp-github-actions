3b070e2a67446a9f6f0228eb15ddd4fed01d8348
pavithratrdev <pavithratr.mca@gmail.com>
2023-11-24 15:26:25 +0530

    changed main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-pavi-project-5678"
   location = "europe-west2"
 }
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-pavithra-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-pavithra-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
